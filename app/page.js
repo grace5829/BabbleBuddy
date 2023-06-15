61f53595b18ae1d625e3e067d275f8d3c6b567ae
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-06-14 20:03:24 -0400

    able to connect to api and translate manually typed phrases.

--- app/page.js
+++ app/page.js
@@ -5,12 +5,46 @@ import { createWorker } from "tesseract.js"
 
 
 export default function Home() {
-const worker = createWorker();
-const [selectedImage, setSelectedImage]= useState(null)
-const [textResult, setTextResult]= useState("")
-const [textTranslated, setTextTranslated]= useState("")
-const [imageLang, setImageLang]= useState("eng")
-const [textLang, setTextLang]= useState("fin")
+  const worker = createWorker();
+  const [selectedImage, setSelectedImage]= useState(null)
+  const [textResult, setTextResult]= useState("")
+  const [textTranslated, setTextTranslated]= useState("")
+  const [imageLang, setImageLang]= useState("eng")
+  const [textLang, setTextLang]= useState("fin")
+  
+  const TextTranslationClient = require("@azure-rest/ai-translation-text").default
+
+  const key = "0ea6074b21c449acb583bb9efdc84d05";
+  let endpoint = "https://api.cognitive.microsofttranslator.com";
+  const location = "eastus";
+
+  const axios = require('axios').default;
+  const { v4: uuidv4 } = require('uuid');
+
+
+axios({
+  baseURL: endpoint,
+  url: '/translate',
+  method: 'post',
+  headers: {
+      'Ocp-Apim-Subscription-Key': key,
+       // location required if you're using a multi-service or regional (not global) resource.
+      'Ocp-Apim-Subscription-Region': location,
+      'Content-type': 'application/json',
+      'X-ClientTraceId': uuidv4().toString()
+  },
+  params: {
+      'api-version': '3.0',
+      'from': 'en',
+      'to': ['sw', 'it']
+  },
+  data: [{
+      'text': 'Hello, friend! What did you do today?'
+  }],
+  responseType: 'json'
+}).then(function(response){
+  console.log(JSON.stringify(response.data, null, 4));
+})
 
 
 const convertImageToText= async () =>{
