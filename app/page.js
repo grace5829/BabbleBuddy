8f14435015d57aa348932f58b8f0ed1445b0bc58
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-06-20 13:02:07 -0400

    able to see selected language from drop down options

--- app/page.js
+++ app/page.js
@@ -5,82 +5,89 @@ import { useEffect, useState } from "react";
 import { createWorker } from "tesseract.js";
 import { languages } from "./languages";
 // import text.png from "./text.png"
-
+import IMAGE_TO_TEXT_KEY from "./.env.local"
+import IMAGE_TO_TEXT_ENDPOINT from "./.env.local"
+import IMAGE_TO_TEXT_LOCATION from "./.env.local"
 
 //create api backend route to translate the lanagues? 
 
 export default function Home() {
   const worker = createWorker();
+  const axios = require("axios").default;
+  const { v4: uuidv4 } = require("uuid");
   const [selectedImage, setSelectedImage] = useState(null);
-  const [textResult, setTextResult] = useState("Upload image");
-  const [textTranslated, setTextTranslated] = useState("Upload image");
+  const [textResult, setTextResult] = useState("Upload image with text");
+  const [textTranslated, setTextTranslated] = useState("");
   const [imageLang, setImageLang] = useState("eng");
   const [textLang, setTextLang] = useState("en");
 
   const TextTranslationClient =
     require("@azure-rest/ai-translation-text").default;
 
-  const key = "0ea6074b21c449acb583bb9efdc84d05";
-  let endpoint = "https://api.cognitive.microsofttranslator.com";
-  const location = "eastus";
 
-  const axios = require("axios").default;
-  const { v4: uuidv4 } = require("uuid");
+
 
   let imageLangOptions = document.getElementById("image-languages");
 
-  console.log(imageLangOptions);
+  // console.log(imageLangOptions);
   // let selectedImageLang=imageLangOptions.options[imageLangOptions.selectedIndex].text;
 
-  axios({
-    baseURL: endpoint,
-    url: "/translate",
-    method: "post",
-    headers: {
-      "Ocp-Apim-Subscription-Key": key,
-      // location required if you're using a multi-service or regional (not global) resource.
-      "Ocp-Apim-Subscription-Region": location,
-      "Content-type": "application/json",
-      "X-ClientTraceId": uuidv4().toString(),
-    },
-    params: {
-      "api-version": "3.0",
-      from: "en",
-      to: ["sw", "it"],
-    },
-    data: [
-      {
-        text: "Hello, friend! What did you do today?",
+  
+  const convertImageTextToSelectedLang = () =>{
+    
+    axios({
+      baseURL: IMAGE_TO_TEXT_ENDPOINT,
+      url: "/translate",
+      method: "post",
+      headers: {
+        "Ocp-Apim-Subscription-Key": IMAGE_TO_TEXT_KEY,
+        // location required if you're using a multi-service or regional (not global) resource.
+        "Ocp-Apim-Subscription-Region": IMAGE_TO_TEXT_LOCATION,
+        "Content-type": "application/json",
+        "X-ClientTraceId": uuidv4().toString(),
       },
-    ],
-    responseType: "json",
-  }).then(function (response) {
-    console.log(JSON.stringify(response.data, null, 4));
-  });
-
+      params: {
+        "api-version": "3.0",
+        from: "en",
+        to: ["sw", "it"],
+      },
+      data: [
+        {
+          text: "Hello, friend! What did you do today?",
+        },
+      ],
+      responseType: "json",
+    }).then(function (response) {
+      console.log(JSON.stringify(response.data, null, 4));
+    });
+ }
+  //image to text api; only works for same language image to same language text
   const languagesKeys = Object.keys(languages);
   const convertImageToText = async () => {
-    await (await worker).loadLanguage("eng");
-    await (await worker).initialize("eng");
+    await (await worker).loadLanguage(imageLang);
+    await (await worker).initialize(imageLang);
     const { data } = await (await worker).recognize(selectedImage);
-    console.log(data.text);
     setTextResult(data.text);
     await (await worker).terminate();
-    // await worker.loadLanguage('eng');
-    // await worker.initialize('eng');
-    // const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    // console.log(text);
-    // await worker.terminate();
   };
 
   useEffect(() => {
     convertImageToText();
-  }, [selectedImage]);
+  }, [selectedImage, imageLang]);
 
   const handleChangeImage = (e) => {
     setSelectedImage(e.target.files[0]);
   };
 
+
+  const handleImageLangChange= (evt) =>{
+    console.log(evt.target.value)
+    let selectedLang=evt.target.value
+    setImageLang(languages[selectedLang].image)
+    // console.log(languages[selectedLang].image)
+  }
+
+  // console.log(imageLang)
   return (
     <main>
       <Link href="/speech">Speech</Link>
@@ -97,7 +104,7 @@ export default function Home() {
       </div>
                       {/* try onChange */}
       <label htmlFor="image-languages">Image Language:</label>
-      <select name="image-languages" id="image-languages">
+      <select name="image-languages" id="image-languages" onChange={handleImageLangChange}>
         {languagesKeys.map((language) => (
           <option value={language} key={language} >
             {language}
@@ -108,12 +115,7 @@ export default function Home() {
       <div>
         {selectedImage == null ? (
           <div>
-            <img
-              src={
-                "https://www.lifewire.com/thmb/tHjH9M19MsA9gFY-qcZvKYv5oG4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cloud-upload-a30f385a928e44e199a62210d578375a.jpg"
-              }
-              alt="thumb"
-            />
+   <p> Upload Image with text</p>
           </div>
         ) : (
           <div>
@@ -122,7 +124,7 @@ export default function Home() {
         )}
 
         <div className="border-2 border-black">
-          <label htmlFor="text-languages" className="border-2  py-px">Image Language:</label>
+          <label htmlFor="text-languages" className="border-2  py-px">Text Language:</label>
 
           <select name="text-languages" id="text-languages" className="border-2 py-px">
             {languagesKeys.map((language) => (
@@ -136,7 +138,9 @@ export default function Home() {
             <div>
               <p>{textResult}</p>
             </div>
-          ) : null}
+          ) :             <div>
+              <p>Upload image with text</p>
+            </div>}
 
         </div>
       </div>
