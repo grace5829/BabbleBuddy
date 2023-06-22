53529b43b08b75d2d23e8b6942ef61208aabd97c
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-06-22 19:28:42 -0400

    final css for image to text

--- app/speech/page.js
+++ app/speech/page.js
@@ -2,65 +2,36 @@
 
 import Link from 'next/link'
 import React, { useState } from 'react'
+// import { Deepgram } from "@deepgram/sdk/browser";
 
 const page = () => {
+  const deepgram = new Deepgram(process.env.NEXT_PUBLIC_SPEECH_TO_TEXT_KEY);
 
-  // const fs = require("fs");
-  // const { Deepgram } = require("@deepgram/sdk");
-  
-  // // Your Deepgram API Key
-  // const deepgramApiKey = "YOUR_DEEPGRAM_API_KEY";
-  
-  // // Location of the file you want to transcribe. Should include filename and extension.
-  // // Example of a local file: ../../Audio/life-moves-pretty-fast.wav
-  // // Example of a remote file: https://static.deepgram.com/examples/interview_speech-analytics.wav
-  // const file = "YOUR_FILE_LOCATION";
-  
-  // // Mimetype for the file you want to transcribe
-  // // Only necessary if transcribing a local file
-  // // Example: audio/wav
-  // const mimetype = "YOUR_FILE_MIME_TYPE";
-  
-  // // Initialize the Deepgram SDK
-  // const deepgram = new Deepgram(deepgramApiKey);
-  
-  // // Check whether requested file is local or remote, and prepare accordingly
-  // if (file.startsWith("http")) {
-  //     // File is remote
-  //     // Set the source
-  //     source = {
-  //         url: file,
-  //     };
-  // } else {
-  //     // File is local
-  //     // Open the audio file
-  //     const audio = fs.readFileSync(file);
-  
-  //     // Set the source
-  //     source = {
-  //         buffer: audio,
-  //         mimetype: mimetype,
-  //     };
-  // }
-  
-  // // Send the audio to Deepgram and get the response
-  // deepgram.transcription
-  //     .preRecorded(source, {
-  //         smart_format: true,
-  //         model: "nova",
-  //     })
-  //     .then((response) => {
-  //         // Write the response to the console
-  //         console.dir(response, { depth: null });
-  
-  //         // Write only the transcript to the console
-  //         //console.dir(response.results.channels[0].alternatives[0].transcript, { depth: null });
-  //     })
-  //     .catch((err) => {
-  //         console.log(err);
-  //     });
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    const mediaRecorder = new MediaRecorder(stream, {
+        mimeType: "audio/webm",
+    });
+const socket = new
 
-      
+    // const deepgramSocket = deepgram.transcription.live({
+    //     punctuate: true,
+    // });
+    // deepgramSocket.addEventListener("open", () => {
+    //     mediaRecorder.addEventListener("dataavailable", async (event) => {
+    //         if (event.data.size > 0 && deepgramSocket.readyState == 1) {
+    //             deepgramSocket.send(event.data);
+    //         }
+    //     });
+    //     mediaRecorder.start(1000);
+    // });
+    // deepgramSocket.addEventListener("message", (message) => {
+    //     const received = JSON.parse(message.data);
+    //     const transcript = received.channel.alternatives[0].transcript;
+    //     if (transcript && received.is_final) {
+    //         console.log(transcript);
+    //     }
+    // });
+});
 
 
 
