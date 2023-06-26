6cbefbf47000e4c6ac92c2144aa8afbcb254ab1a
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-06-26 19:18:57 -0400

    removed speech page

--- app/page.js
+++ app/page.js
@@ -3,16 +3,16 @@
 import {ThemeProvider} from 'next-themes'
 import {useTheme} from "next-themes"
 import Link from "next/link";
-
+import ImagePage from './image/page';
 export default function Home() {
   const {theme, setTheme}=useTheme()
   return (
     <ThemeProvider>
 
     <main>
-     <Link href="/image">Upload Image</Link>
-     <Link href="/speech" className="mx-1.5">Record Voice</Link>
-
+     {/* <Link href="/image">Upload Image</Link> */}
+     {/* <Link href="/speech" className="mx-1.5">Record Voice</Link> */}
+    <ImagePage/>
    
     </main>
     </ThemeProvider>
